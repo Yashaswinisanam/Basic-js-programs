9513a8c655d9181d500d6c654ac37d0feac788f5
unknown <sanamyashaswini@gmail.com>
2026-05-04 14:29:23 +0530

    switchcaseprogram users input

---
+++ switch.js
@@ -0,0 +1,23 @@
+let days="2";
+
+switch(days)
+{
+    case "1":
+        console.log("sunday");
+        break;
+    case "2":
+        console.log("tuesday");
+        break;
+    case "3":
+        console.log("wednesday");
+        break;
+    case "4":
+        console.log("thursday");
+        break;
+    case "5":
+        console.log("friday");
+        break;
+     case "6":
+        console.log("saturday");
+        break;
+}
